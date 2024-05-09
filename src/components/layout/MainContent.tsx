280ca9a3f502ce8e42a0831111d7f4802f601722
A4va <a.k.a.502.2000@gmail.com>
2024-05-09 07:03:44 +0300

    💄 style: add cards badges

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import imgUrl from "../../assets/placeholder.svg";
+// import imgUrl from "../../assets/placeholder.svg";
 import { useAppSelector } from "../store/hooks";
 import { ScrollArea } from "../ui/scroll-area";
 import {
   Card,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { DollarSign, MapPin } from "lucide-react";
+import { DollarSign, MapPin, Star } from "lucide-react";
 import { Link } from "react-router-dom";
 import { data } from "../../data/hotels-data.json";
 import { Badge } from "../ui/badge";
@@ -29,7 +28,7 @@ export default function MainContent() {
 
   function filterData(value: string) {
     const lowerCaseValue = value.trim().toLowerCase();
-    
+
     return data.filter(({ name, location }) => {
       const lowerCaseName = name.toLowerCase();
       const lowerCaseLocation = location.toLowerCase();
@@ -58,34 +57,39 @@ export default function MainContent() {
       <ScrollArea className="whitespace-nowrap rounded-md sm:border">
         <div className="grid grid-cols-2 gap-4 empty:h-96 md:p-4 lg:grid-cols-3 xl:grid-cols-5">
           {searchResults.map((hotel) => (
-            <Card key={hotel.id}>
               <Link to={`/hotels/${hotel.id}`}>
+            <Card className="relative" key={hotel.id}>
                 <img
-                  alt="Gallery Image 1"
+                  alt={`${hotel.name}-${hotel.id}`}
                   className="w-full overflow-hidden rounded-t-lg object-cover"
                   width="300"
                   height="300"
-                  src={imgUrl}
+                  src={hotel.imgs[0]}
                   style={{
                     aspectRatio: "300/300",
                     objectFit: "cover",
                   }}
                 />
-              </Link>
-              <CardHeader className="p-4">
+              <CardHeader className="flex items-center-between px-2 pt-2 pb-8">
                 <CardTitle>{hotel.name}</CardTitle>
-                <CardDescription className="flex items-center gap-0.5">
-                  <MapPin size={18} strokeWidth="1.4" />
+                <Badge className="absolute top-2 left-0 flex w-fit items-center gap-0.5 rounded-s-none">
+                  <MapPin size={10} strokeWidth="1.4" />
                   {hotel.location}
-                </CardDescription>
+                </Badge>
               </CardHeader>
-              <CardFooter className="px-0 py-0">
-                <Badge className="flex w-fit items-center gap-0.5 rounded-ss-none bg-emerald-600 px-2.5 py-0.5 hover:bg-emerald-700 dark:bg-emerald-300 hover:dark:bg-emerald-400">
+              <CardFooter className="flex justify-between px-0 py-0">
+                <Badge className="flex shadow-2xl w-fit items-center gap-0.5 rounded-ss-none bg-emerald-600 px-2.5 py-0.5 hover:bg-emerald-700 dark:bg-emerald-300 hover:dark:bg-emerald-400">
                   <DollarSign size={10} strokeWidth="2" />
                   {hotel.price}
                 </Badge>
+
+                <Badge className="flex w-fit items-center gap-0.5 rounded-bl-none rounded-tr-none bg-yellow-600 px-2.5 py-0.5 hover:bg-yellow-700 dark:bg-yellow-300 hover:dark:bg-yellow-400">
+                  <Star fill="white" size={10} strokeWidth="2" />
+                  {hotel.rating}
+                </Badge>
               </CardFooter>
             </Card>
+              </Link>
           ))}
         </div>
       </ScrollArea>
